a1f4d24b8954269574c321c6187e62f5231ac03f
articsaber <kaarthiknirmala@gmail.com>
2024-10-30 13:19:59 +0530

    new changes

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello All, Happy Learning -- Updated Version'));
+app.get("/", (req, res) => res.send("Say Hello to the World!"));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
